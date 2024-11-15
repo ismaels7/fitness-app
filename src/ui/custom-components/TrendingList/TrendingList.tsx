02e03d5cd7b6d68efc3c1cfe885ef5a1ee3a1988
Ismael <isma191101@gmail.com>
2024-11-15 14:20:59 +0100

    homepage and components v1

---
+++ src/ui/custom-components/TrendingList/TrendingList.tsx
@@ -0,0 +1,31 @@
+import { DataListItem, DataList, Icon, Box, GridItem, Heading } from "@chakra-ui/react"
+import { FaAngleRight } from "react-icons/fa6"
+
+type ItemListType = {
+    id: string,
+    name: string
+}
+
+type TrendingListProps = {
+    list: ItemListType[]
+}
+export const TrendingList = ({ list }: TrendingListProps) => {
+    return (
+        <GridItem >
+            <Heading display={"flex"} justifyContent={"center"} alignItems={"center"} >Trending exercses</Heading>
+            <Box display={"flex"} justifyContent={"center"} alignItems={"center"} height={"100%"}>
+                <div>
+            {list.map((item) => {
+                return (
+                    <div className="flex items-center">
+                        <Icon fontSize={"20px"}><FaAngleRight /></Icon>
+                        <h3>{item.name}</h3>
+                    </div>
+                )
+            })}
+            </div>
+            </Box>
+           
+        </GridItem>
+    )
+}
